3162105a9724708f51ece49bdf44ed39b7911429
AngelAltur <125256303+AngelAltur@users.noreply.github.com>
2023-09-28 12:56:51 +0200

    0.3.1

--- WhatsDAMFroyo/app/src/main/java/com/ieseljust/pmdm/whatsdamfroyo/MainActivity.kt
+++ WhatsDAMFroyo/app/src/main/java/com/ieseljust/pmdm/whatsdamfroyo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ieseljust.pmdm.whatsdamfroyo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Nickname
 import com.ieseljust.pmdm.whatsdamfroyo.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
@@ -13,12 +14,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
-        binding.buttonConnect.setOnClickListener{
-            val intent= Intent(this, MessagesWindow::class.java)
+        binding.buttonConnect.setOnClickListener {
+
+            val intent = Intent(this, MessagesWindow::class.java)
             startActivity(intent)
+
+
         }
 
     }
-
-
 }
