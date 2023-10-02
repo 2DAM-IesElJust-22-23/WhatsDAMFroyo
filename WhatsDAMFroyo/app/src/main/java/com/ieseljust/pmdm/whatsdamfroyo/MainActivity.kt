7326d343ded40d01701ed198e3b742dcfb6efa84
Ruben Altur Bononad <rubenaltur2883@gmail.com>
2023-10-02 19:49:27 +0200

    2/10

--- WhatsDAMFroyo/app/src/main/java/com/ieseljust/pmdm/whatsdamfroyo/MainActivity.kt
+++ WhatsDAMFroyo/app/src/main/java/com/ieseljust/pmdm/whatsdamfroyo/MainActivity.kt
@@ -1,23 +1,44 @@
 package com.ieseljust.pmdm.whatsdamfroyo
 
 import android.content.Intent
+import android.net.InetAddresses.isNumericAddress
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract.CommonDataKinds.Nickname
+import androidx.annotation.RequiresApi
 import com.ieseljust.pmdm.whatsdamfroyo.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private var usuari=""
+    private var ipservidor=""
 
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        if(savedInstanceState!=null){
+            usuari=savedInstanceState.getString("usuari","")
+            ipservidor=savedInstanceState.getString("ipservidor","")
+        }
+
+
 
         binding.buttonConnect.setOnClickListener {
 
             val intent = Intent(this, MessagesWindow::class.java)
-            startActivity(intent)
+            usuari=binding.nickNameText.text.toString()
+            ipservidor=binding.serverAddressText.text.toString()
+
+            if((usuari.isNotEmpty()) and isNumericAddress(ipservidor)){
+                intent.putExtra("usuari", usuari)
+                intent.putExtra("ipservidor", ipservidor)
+                startActivity(intent)
+            }
+
+
 
 
         }
--- WhatsDAMFroyo/app/src/main/java/com/ieseljust/pmdm/whatsdamfroyo/MessagesWindow.kt
+++ WhatsDAMFroyo/app/src/main/java/com/ieseljust/pmdm/whatsdamfroyo/MessagesWindow.kt
@@ -2,9 +2,32 @@ package com.ieseljust.pmdm.whatsdamfroyo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ieseljust.pmdm.whatsdamfroyo.databinding.ActivityMessagesWindowBinding
 
 class MessagesWindow : AppCompatActivity() {
+    private lateinit var  binding: ActivityMessagesWindowBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding=ActivityMessagesWindowBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val textMensaje=binding.MessageText
+        val textview= binding.connectionInfoTextView
+        val enviarMensaje= binding.sendMessage
+
+        val usuari=intent.getStringExtra("usuari")
+        val ipServidor=intent.getStringExtra("ipservidor")
+
+        textview.text="Conectat a $ipServidor com a $usuari"
+        enviarMensaje.setOnClickListener{
+            textMensaje.text.clear()
+        }
     }
+
+
+
+
+
+
+
 }
